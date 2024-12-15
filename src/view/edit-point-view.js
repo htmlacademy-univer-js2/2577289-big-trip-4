e0de1dd0ac7d989794595e4106951fb612dd7d43
Churina Margaery <elshanskaya.r@inbox.ru>
2024-12-15 16:45:28 +0300

    view upgrade

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -1,35 +1,91 @@
 import {createEditPointTemplate} from '../template/edit-point-template.js';
-import AbstractView from '../framework/view/abstract-view.js';
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
+import { findOffersByType, findDestinationId } from '../mock/point.js';
 
-export default class EditPointView extends AbstractView {
-  #point = null;
+export default class EditPointView extends AbstractStatefulView {
   #handleFormSubmit = null;
   #handleButtonClick = null;
 
   constructor({point, onFormSubmit, onButtonClick}) {
     super();
-    this.#point = point;
+    this._setState(EditPointView.parsePointToState(point));
     this.#handleFormSubmit = onFormSubmit;
     this.#handleButtonClick = onButtonClick;
+    this._restoreHandlers();
+  }
 
+  get template() {
+    return createEditPointTemplate(this._state);
+  }
+
+  _restoreHandlers() {
     this.element.querySelector('form') // event /+ event--edit
       .addEventListener('submit', this.#formSubmitHandler);
 
     this.element.querySelector('.event__rollup-btn')
       .addEventListener('click', this.#buttonClickHandler);
+
+    this.element.querySelector('.event__type-group')
+      .addEventListener('change', this.#typeChangeHandler);
+
+    const dests = this.element.querySelectorAll('.event__input--destination');
+    for(const dest of dests) {
+      dest.addEventListener('change', this.#destChangeHandler);
+    }
+
+    const container = this.element.querySelector('.event__section--offers');
+    const checkboxes = container.querySelectorAll('.event__offer-checkbox');
+    for(const checkbox of checkboxes) {
+      checkbox.addEventListener('click', this.#offerClickHandler);
+    }
   }
 
-  get template() {
-    return createEditPointTemplate(this.#point);
-  }
+  #destChangeHandler = (evt) => {
+    evt.preventDefault();
+    this.updateElement({
+      destination: findDestinationId(evt.target.value).id,
+    });
+  };
+
+  #offerClickHandler = (evt) => {
+    evt.preventDefault();
+    const value = evt.target.value;
+    let offersCopy = [...this._state.offers];
+    if (evt.target.checked) {
+      if (!offersCopy.includes(value)) {
+        offersCopy.push(value);
+      }
+    } else {
+      offersCopy = offersCopy.filter((item) => item !== value);
+    }
+    this.updateElement({
+      offers: offersCopy,
+    });
+  };
+
+  #typeChangeHandler = (evt) => {
+    evt.preventDefault();
+    this.updateElement({
+      type: evt.target.value,
+      offers: findOffersByType(evt.target.value),
+    });
+  };
 
   #formSubmitHandler = (evt) => {
     evt.preventDefault();
-    this.#handleFormSubmit(this.#point);
+    this.#handleFormSubmit(this._state);
   };
 
   #buttonClickHandler = (evt) => {
     evt.preventDefault();
     this.#handleButtonClick();
   };
+
+  static parsePointToState(task) {
+    return {...task};
+  }
+
+  static parseStateToPoint(state) {
+    return {...state};
+  }
 }
