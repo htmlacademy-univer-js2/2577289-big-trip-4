892f9e73a880b01adfe2bbdf3013dc38984a3b97
Churina Margaery <elshanskaya.r@inbox.ru>
2025-01-04 21:14:45 +0300

    templates finished

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import FilterModel from './model/filter-model.js';
 const bodyElement = document.querySelector('body');
 const headerElement = bodyElement.querySelector('.page-header');
 const tripInfoElement = headerElement.querySelector('.trip-main');
-// const filterElement = tripInfoElement.querySelector('.trip-controls__filters');
 const mainElement = bodyElement.querySelector('.page-main');
 const eventListElement = mainElement.querySelector('.trip-events');
 
